3a45e662b774ae1c6fd7e6b0b4f3ec8bc1000e18
BunsinNANH <bunsinnanh22@gmail.com>
2019-06-11 17:17:31 +0700

    third commit

--- App.js
+++ App.js
@@ -14,33 +14,51 @@ import {Platform,
   ImageBackground,
   TextInput,
 } from 'react-native';
-// import axios from 'axios';
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import axios from 'axios';
+
 const API_KEY = 'df321a026c2f5245320b089d52dcedb3';
+const DEFAULT_ZIPCODE = 90210;
 const cityName = [
-  'Phnom Penh \n',
-  'Bang Kok \n',
-  'New York \n',
-  'China \n',
-  'Paris \n'
+  'Phnom Penh',
+  'Bang Kok',
+  'New York',
+  'China',
+  'Paris'
 ];
 type Props = {};
 export default class App extends Component<Props> {
+  constructor(){
+    super();
+    this.state={
+      zipcode: DEFAULT_ZIPCODE,
+      days:[],
+    }
+  }
+  _getForecast(cityName){
+    
+    const request_url = "https://api.openweathermap.org/data/2.5/weather?q="+cityName+",&appid="+API_KEY;
+    axios.get(request_url).then((response)=>{
+      if( response.status == 200){
+        console.log(response.data);
+      }
+    });
+  }
+  
   render() {
+    // if( this.state.days.length<=0){
+    //   this._getForecast(this.state.cityName);
+    // }
     return (
       <ImageBackground source={ require('./background/images/bluesky.jpg')}
       style={styles.container}>
         <View style={styles.inner}>
-          <Text style={styles.cityName}>{cityName}</Text>
+          <Text style={styles.cityName}>Phnom Penh</Text>
           <Text style={styles.weatherCondition}>Blue Sky</Text>
           <Text style={styles.temperature}>27</Text>
-          <TextInput style={styles.searchCity} placeholder="Search City"></TextInput>
-          
+          <TextInput ref= "cityname"
+          onChangeText={(text) => this.setState({text})}
+                  value={this.state.cityName} style={styles.searchCity} 
+                  placeholder="Search City"></TextInput>
         </View>
       </ImageBackground>
     );
@@ -62,11 +80,11 @@ const styles = StyleSheet.create({
     paddingBottom: 5,
   },
   searchCity: {
-    color: '#fff',
+    color: '#000',
     borderWidth: 1,
     borderColor: '#fafafa',
     backgroundColor:'#f5f5f5',
-    width: 200,
+    width: 250,
     height: 30,
     borderRadius: 10,
     paddingLeft: 10,
