1068fc97323e6bcb0787262d846c89e9702018a0
BunsinNANH <bunsinnanh22@gmail.com>
2019-06-12 13:21:36 +0700

    Weather app

--- App.js
+++ App.js
@@ -7,6 +7,7 @@
  */
 
 import React, {Component} from 'react';
+// import getImageBackground from './weather/getImageBackground';
 import {Platform, 
   StyleSheet, 
   Text, 
@@ -15,28 +16,61 @@ import {Platform,
   TextInput,
 } from 'react-native';
 import axios from 'axios';
-
+function funchange(){
+  console.log(this);
+}
 const API_KEY = 'df321a026c2f5245320b089d52dcedb3';
 const DEFAULT_ZIPCODE = 90210;
-const cityName = [
-  'Phnom Penh',
-  'Bang Kok',
-  'New York',
-  'China',
-  'Paris'
-];
+
 type Props = {};
 export default class App extends Component<Props> {
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
     this.state={
-      zipcode: DEFAULT_ZIPCODE,
-      days:[],
+      cityname:'Phnom Penh',
     }
+    this.handlechangeText = this.handlechangeText.bind(this);
   }
-  _getForecast(cityName){
+  state = {
+    loading: false,
+    error : false,
+    location: '',
+    temperature:0,
+    weather: ""
+  }
+  componentDidMount(){
+    this.handlechangeText('Phnom Penh');
+  }
+  handlechangeText = async cityname =>{
+    // this state
+    if(!cityname) return;
+      this.setState({
+        loading:true,
+        location: cityname,}, async () =>{
+          try{
+            const locationId = await fetchLocationId(cityname);
+            const { location, weather , temperature} = await fetchWeather(
+              locationId
+            );
+            this.setState({
+              loading: false,
+              error : false,
+              location,
+              temperature,
+              weather
+            });
+          } catch (e){
+            this.setState({
+              loading: false,
+              error : true,
+            })
+          }
+        }
+     )
+  }
+  _getForecast(cityname){
     
-    const request_url = "https://api.openweathermap.org/data/2.5/weather?q="+cityName+",&appid="+API_KEY;
+    const request_url = "https://api.openweathermap.org/data/2.5/weather?q="+ cityname+"&units=metric"+ "&APPID="+ API_KEY;
     axios.get(request_url).then((response)=>{
       if( response.status == 200){
         console.log(response.data);
@@ -45,20 +79,22 @@ export default class App extends Component<Props> {
   }
   
   render() {
-    // if( this.state.days.length<=0){
-    //   this._getForecast(this.state.cityName);
+    // if( this.state.cityname.length<=0){
+    //   this._getForecast(this.state);
+    //   console.log(this.state);
     // }
     return (
       <ImageBackground source={ require('./background/images/bluesky.jpg')}
       style={styles.container}>
         <View style={styles.inner}>
-          <Text style={styles.cityName}>Phnom Penh</Text>
+          <Text style={styles.cityName}>{this.state.cityname}</Text>
           <Text style={styles.weatherCondition}>Blue Sky</Text>
           <Text style={styles.temperature}>27</Text>
           <TextInput ref= "cityname"
-          onChangeText={(text) => this.setState({text})}
-                  value={this.state.cityName} style={styles.searchCity} 
-                  placeholder="Search City"></TextInput>
+                  onChangeText={this.handlechangeText}
+                  style={styles.searchCity} 
+                  placeholder="Search City">
+          </TextInput>
         </View>
       </ImageBackground>
     );
